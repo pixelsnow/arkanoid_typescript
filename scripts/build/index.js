98e1da0c1c14d198811c2f8cca773b2ca1b2ccb9
Valeria <lera.vagapova@gmail.com>
2017-01-27 05:40:39 +0300

    removed build

--- scripts/build/index.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports", "./Game/index"], function (require, exports, index_1) {
-    "use strict";
-    /**
-     * Индексный модуль приложения
-     */
-    ;
-    index_1.default();
-});
-//# sourceMappingURL=index.js.map
